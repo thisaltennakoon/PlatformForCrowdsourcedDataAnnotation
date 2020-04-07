248617e5efcb6125d451f9455b62f449172f1873
Thisal Tennakoon <42693357+thisaltennakoon@users.noreply.github.com>
2020-04-07 16:48:55 +0530

    Initial migrate after creating apps

--- CrowdsourcedDataAnnotationPlatform/settings.py
+++ CrowdsourcedDataAnnotationPlatform/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'CreateDataAnnotationTask',
+    'CreateDataGenerationTask',
+    'DoDataAnnotationTask',
+    'DoDataGenerationTask',
+    'UserManagement',
 ]
 
 MIDDLEWARE = [
