21fbd1d2e066c3433d1f311e72c785d375cc04af
Thisal Tennakoon <42693357+thisaltennakoon@users.noreply.github.com>
2020-04-07 20:25:36 +0530

    class DataAnnotationResult

--- DoDataAnnotationTask/models.py
+++ DoDataAnnotationTask/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class DataAnnotationResult(models.Model):
+    TaskId = models.IntegerField(null=False,blank=False)
+    DataInstance = models.CharField(max_length=30, blank=False)
+    ClassName = models.CharField(max_length=100, blank=False)
+    UserID = models.IntegerField(null=False,blank=False)
+
+    #def __str__(self):  # display book name in admin panel
+        #return 'TaskID:'+str(self.TaskId) + ' DataInstance:' + self.DataInstance + ' UserID:'+self.UserID + ' ClassName:' + self.ClassName
