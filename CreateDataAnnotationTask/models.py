0eaf144d8af4cd3a1a20965cdf8ce13036556b59
Kasun Tharaka <mornk1@gmail.com>
2020-04-10 19:30:23 +0530

    Task model added

--- CreateDataAnnotationTask/models.py
+++ CreateDataAnnotationTask/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=250)
+
+class AnnotationTask(models.Model):
+    creatorID = models.ForeignKey(User,on_delete=models.CASCADE)
+    TaskName = models.CharField(max_length=250)
+    TaskDecription = models.CharField(max_length=1000)
+    TaskStatus = models.CharField(max_length=60, default='new')    #new,#inprogress,#completed
+
+
