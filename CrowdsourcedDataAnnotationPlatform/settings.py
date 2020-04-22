ca247cc613baa356461e3dae7d8399dae38930c5
Thisal Tennakoon <42693357+thisaltennakoon@users.noreply.github.com>
2020-04-22 12:30:04 +0530

    Text Highlighted

--- CrowdsourcedDataAnnotationPlatform/settings.py
+++ CrowdsourcedDataAnnotationPlatform/settings.py
@@ -80,11 +80,19 @@ WSGI_APPLICATION = 'CrowdsourcedDataAnnotationPlatform.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'CrowdsourcedDataAnnotationPlatform',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST':'localhost'
     }
 }
-
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
