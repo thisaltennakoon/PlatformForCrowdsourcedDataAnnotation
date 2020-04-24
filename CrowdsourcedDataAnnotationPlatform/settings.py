5291415908440ace6fe4699ea83fec19b1f56ce8
JananiSudeeptha <jananisudeeptha@gmail.com>
2020-04-24 16:45:47 +0530

    User Management added

--- CrowdsourcedDataAnnotationPlatform/settings.py
+++ CrowdsourcedDataAnnotationPlatform/settings.py
@@ -80,13 +80,11 @@ WSGI_APPLICATION = 'CrowdsourcedDataAnnotationPlatform.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'CrowdsourcedDataAnnotationPlatform',
-        'USER': 'postgres',
-        'PASSWORD': '1234',
-        'HOST':'localhost'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 #DATABASES = {
 #    'default': {
 #        'ENGINE': 'django.db.backends.sqlite3',
