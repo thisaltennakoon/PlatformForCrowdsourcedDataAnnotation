fa56cf3fd75f936f43d98d0803da19abb7633415
thisaltennakoon <thisaltennakoon@gmail.com>
2020-04-25 03:08:26 +0530

    models modified

--- CrowdsourcedDataAnnotationPlatform/settings.py
+++ CrowdsourcedDataAnnotationPlatform/settings.py
@@ -78,19 +78,24 @@ WSGI_APPLICATION = 'CrowdsourcedDataAnnotationPlatform.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+}"""
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'CrowdsourcedDataAnnotationPlatform',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST':'localhost'
+    }
 }
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
