3de4d3259099b6209e16999b682695034791221b
Abira16 <abinss.17@cse.mrt.ac.lk>
2020-05-28 19:12:57 +0530

    delete my folders from master branch

--- ImageDataAnalyse/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ImagedataanalyseConfig(AppConfig):
-    name = 'ImageDataAnalyse'
--- testresultrank/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TestresultrankConfig(AppConfig):
-    name = 'testresultrank'
