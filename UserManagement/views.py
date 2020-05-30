02912136d9c0e3df9e70b4619963dabb80051fb1
thisaltennakoon <thisaltennakoon@gmail.com>
2020-05-30 12:22:04 +0530

    Usermangement update

--- UserManagement/views.py
+++ UserManagement/views.py
@@ -1,29 +1,29 @@
-from django.contrib import messages
-from django.contrib.auth import (authenticate, login, logout,
-                                 update_session_auth_hash)
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import (AuthenticationForm, UserCreationForm,
-                                       PasswordChangeForm)
-from django.urls import reverse
-from django.http import HttpResponseRedirect,HttpResponse
-from django.shortcuts import render, redirect, get_object_or_404
-
-from . import models
-from . import forms
 from .models import ContributorTask
-from CreateDataGenerationTask.models import Task as DataGenerationTask
-from CreateDataAnnotationTask.models import Task as DataAnnotationTask
+from django.contrib import messages
+from django.contrib.auth import (authenticate, login, logout,update_session_auth_hash)
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import (AuthenticationForm, UserCreationForm,PasswordChangeForm)
+from django.contrib.auth.models import Group, User
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect, get_object_or_404
+from . import models
+from .forms import CreateUserForm, SigInForm, PWChangeForm
+from.filters import ProfileFilter
+from .forms import ProfileForm # RateForm
+from .models import Profile
+from django.contrib import messages
+
 
 def sign_in(request):
-    form = AuthenticationForm()
-    username= 'not logged in'
+    form = SigInForm()
+    username = 'not logged in'
     if request.method == 'POST':
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
-            if form. user_cache is not None:
+            if form.user_cache is not None:
                 user = form.user_cache
                 username = form.cleaned_data['username']
-                request.session['username'] = username
                 request.session['user_id'] = user.id
                 if user.is_active:
                     login(request, user)
@@ -50,20 +50,19 @@ def formView(request):
       return render(request, 'sign_in.html', {})
 
 def sign_up(request):
-    form = UserCreationForm()
+    form = CreateUserForm()
     if request.method == 'POST':
-        form = UserCreationForm(data=request.POST)
+        form = CreateUserForm(data=request.POST)
         if form.is_valid():
-            form.save()
+            new_user = form.save()
             user = authenticate(
                 username=form.cleaned_data['username'],
                 password=form.cleaned_data['password1']
             )
+            group = Group.objects.get(name='crowd_user')
+            new_user.groups.add(group)
             login(request, user)
-            messages.success(
-                request,
-                "You're now a user! You've been signed in, too."
-            )
+            messages.success(request,"Congradulations! Your account was created successfully")
             return HttpResponseRedirect(reverse('home'))  # TODO: go to profile
     return render(request, 'UserManagement/sign_up.html', {'form': form})
 
@@ -78,13 +77,12 @@ def sign_out(request):
     return HttpResponseRedirect(reverse('home'))
 
 
-@login_required(login_url='UserManagement:sign_in')
+@login_required (login_url='UserManagement:sign_in')
 def profile(request):
     """Display User Profile"""
     profile = request.user.profile
-
     return render(request, 'UserManagement/profile.html', {
-        'profile': profile,
+        'profile': profile
     })
 
 
@@ -92,14 +90,14 @@ def profile(request):
 def edit_profile(request):
     user = request.user
     profile = get_object_or_404(models.Profile, user=user)
-    form = forms.ProfileForm(instance=profile)
+    form = ProfileForm(instance=profile)
 
     if request.method == 'POST':
-        form = forms.ProfileForm(instance=profile, data=request.POST)
+        form = ProfileForm(request.POST, request.FILES, instance=profile  )
         if form.is_valid():
             form.save()
             messages.success(request, "Updated the Profile Successfully!")
-            return HttpResponseRedirect(reverse('accounts:profile'))
+            return HttpResponseRedirect(reverse('UserManagement:profile'))
 
     return render(request, 'UserManagement/edit_profile.html', {
         'form': form
@@ -109,17 +107,48 @@ def edit_profile(request):
 @login_required(login_url='UserManagement:sign_in')
 def change_password(request):
     if request.method == 'POST':
-        form = PasswordChangeForm(request.user, request.POST)
+        form = PWChangeForm(request.user, request.POST)
         if form.is_valid():
             user = form.save()
             update_session_auth_hash(request, user)
             messages.success(request, 'Your password was successfully updated!')
             return HttpResponseRedirect(reverse('UserManagement:profile'))
     else:
-        form = PasswordChangeForm(request.user)
+        form = PWChangeForm(request.user)
     return render(request, 'UserManagement/change_password.html', {
         'form': form
     })
+
+
+def search (request):
+    profile_list = Profile.objects.all().exclude(first_name = '')
+    searchFilter = ProfileFilter(request.GET, queryset=profile_list)
+    return render(request, 'UserManagement/search.html', {'filter':searchFilter})
+
+def profiles(request):
+    profiles = Profile.objects.all().exclude(first_name = '')
+    return render(request,'UserManagement/profile_list.html', {'profiles':profiles})
+
+def view_profile(request, pk):
+    profile = Profile.objects.get(user=pk)
+    context = {'profile': profile}
+    return render(request, 'UserManagement/view_profile.html',context)
+
+def delete_profile(request, pk):
+    profile = Profile.objects.get(user=pk)
+    user = User.objects.get(id=pk)
+    context = {'profile': profile}
+    if request.method == "POST":
+        profile.delete()
+        user.delete()
+        #return redirect('UserManagement/profile_list.html')
+    return render(request, 'UserManagement/delete_profile.html', context)
+
+"""def rate(request):
+    if request.method == "POST":
+        form = RateForm()
+        if form.is_valid():
+            rate = form.save()"""
 
 @login_required(login_url='UserManagement:sign_in')
 def view_my_tasks(request):
