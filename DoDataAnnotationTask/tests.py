854f50ab32366d07449cb8965f3e6a76287b8fc9
thisaltennakoon <thisaltennakoon@gmail.com>
2020-06-08 23:16:53 +0530

    unit testing for urls

--- DoDataAnnotationTask/tests.py
+++ DoDataAnnotationTask/tests.py
@@ -1,3 +1,42 @@
-from django.test import TestCase
+from django.test import TestCase,Client
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from .views import *
+from .models import *
 
-# Create your tests here.
+
+class TestUrls(SimpleTestCase):
+
+    def test_test_url_is_resolved(self):
+        url = reverse('test')
+        self.assertEquals(resolve(url).func, test)
+
+    def test_task_url_is_resolved(self):
+        url = reverse('task')
+        self.assertEquals(resolve(url).func, task)
+
+    def test_view_my_annotations_url_is_resolved(self):
+        url = reverse('view_my_annotations')
+        self.assertEquals(resolve(url).func, view_my_annotations)
+
+    def test_view_my_annotations_change_url_is_resolved(self):
+        url = reverse('view_my_annotations_change')
+        self.assertEquals(resolve(url).func, view_my_annotations_change)
+
+    def test_skip_data_instance_url_is_resolved(self):
+        url = reverse('skip_data_instance')
+        self.assertEquals(resolve(url).func, skip_data_instance)
+
+    def test_stop_annotating_url_is_resolved(self):
+        url = reverse('stop_annotating')
+        self.assertEquals(resolve(url).func, stop_annotating)
+
+
+class TestViews(TestCase):
+
+    def test_testgtgt_GET(self):
+        client = Client()
+        #response = client.get(reverse('test'))
+
+        #self.assertEquals(response.status_code, 200)
+        #self.assertTemplateUsed(response, 'DoDataAnnotationTask/test.html')
--- DoDataAnnotationTask/urls.py
+++ DoDataAnnotationTask/urls.py
@@ -3,11 +3,11 @@ from . import views
 
 urlpatterns = [
     #path('', views.first),
-    path('test', views.test),
-    path('Task', views.task),
-    path('ViewMyAnnotations',views.view_my_annotations),
-    path('ViewMyAnnotations/Change', views.view_my_annotations_change),
-    path('SkipDataInstance', views.skip_data_instance),
-    path('StopAnnotating', views.stop_annotating),
+    path('test', views.test, name='test'),
+    path('Task', views.task, name='task'),
+    path('ViewMyAnnotations',views.view_my_annotations, name='view_my_annotations'),
+    path('ViewMyAnnotations/Change', views.view_my_annotations_change, name='view_my_annotations_change'),
+    path('SkipDataInstance', views.skip_data_instance, name='skip_data_instance'),
+    path('StopAnnotating', views.stop_annotating, name='stop_annotating'),
     #path('ViewMyAnnotations/Delete', views.view_my_annotations_delete),
 ]
