659358a94efefccdcf785bf551c63a439fd5d156
Thisal Tennakoon <42693357+thisaltennakoon@users.noreply.github.com>
2020-09-01 11:51:34 +0530

    Update models.py

--- DoTextDataAnnotationTask/models.py
+++ DoTextDataAnnotationTask/models.py
@@ -4,7 +4,6 @@ from CreateTask.models import Task,TextDataInstance
 
 class DataAnnotationResult(models.Model):
     TaskID = models.ForeignKey(Task ,related_name='textanno' ,on_delete=models.CASCADE)
-    #DataInstance = models.CharField(max_length=30, blank=False)
     DataInstance = models.ForeignKey(TextDataInstance, on_delete=models.CASCADE)
     ClassID = models.IntegerField(default=0, null=False, blank=False)
     UserID = models.IntegerField(null=False,blank=False)
